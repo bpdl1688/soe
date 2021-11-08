f4075cd771f0c6f46e4fbd86137fab9c38f370ae
BPDL <73742154+bpdl1688@users.noreply.github.com>
2021-11-08 10:40:56 +0800

    Create init-centos-stream.sh

---
+++ init-centos-stream.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# for centos stream 8
+# run under user centos
+
+# parameters
+HOSTNAME=r1s18
+
+# Update sources
+sudo dnf update -y
+
+# my SOP
+sudo dnf install epel-release wget tmux vim bind-utils git -y
+
+########################################
+# basic setup
+########################################
+echo "config system"
+
+echo "config timezone"
+sudo timedatectl set-timezone Asia/Singapore
+sudo echo "autocmd FileType yaml setlocal ts=2 sts=2 sw=2 expandtab" > ~/.vimrc
+echo "autocmd FileType yaml setlocal ts=2 sts=2 sw=2 expandtab" > ~/.vimrc
+
+echo "config hostname"
+sudo hostnamectl set-hostname $HOSTNAME
+
+########################################
+# optional software
+########################################
+
+# ansible (optional)
+# sudo dnf install ansible -y
+
+# docker and docker-compose (optional)
+# sudo dnf install docker docker-compose -y
+# sudo systemctl start docker
+# sudo systemctl enable docker
+
+# podman (optional)
+sudo dnf install podman -y
+
+# echo "rebooting..."
+# reboot
